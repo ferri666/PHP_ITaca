7013b2f2013098ae38b3ded359086f5f08b4983b
ferri666 <78905276+ferri666@users.noreply.github.com>
2021-04-21 16:32:39 +0200

    Add files via upload

--- Nivell3.php
+++ Nivell3.php
@@ -12,22 +12,39 @@ $y=4;
 $n=50.78;
 $m=10.34;
 
+///Ensenyar les Variables
 echo"<p><b>Valor de les Variables:</b></p>";
 echo"<p><b>X</b>=".$x.", <b><b>Y</b></b>=".$y.", <b><b>N</b></b>=".$n.", <b><b>M</b></b>=".$m."</p>";
+
+///Suma de les Variables
 echo"<p><b>Suma de les Variables:</b></p>";
 echo"<p><b>X</b>+<b>Y</b>=".$x+$y.", <b>N</b>+<b>M</b>=".$n+$m."</p>";
+
+//Resta de les Variables
 echo"<p><b>Resta de les Variables:</b></p>";
 echo"<p><b>X</b>-<b>Y</b>=".$x-$y.", <b>N</b>-<b>M</b>=".$n-$m."</p>";
+
+//Producte de les variables
 echo"<p><b>Producte de les Variables:</b></p>";
 echo"<p><b>X</b>*<b>Y</b>=".$x*$y.", <b>N</b>*<b>M</b>=".$n*$m."</p>";
+
+//Divisió de les Variables
 echo"<p><b>Divisió de les Variables:</b></p>";
 echo"<p><b>X</b>/<b>Y</b>=".$x/$y.", <b>N</b>/<b>M</b>=".$n/$m."</p>";
+
+//Módul de les Variables
 echo"<p><b>Módul de les Variables:</b></p>";
 echo"<p><b>X</b>%<b>Y</b>=".$x%$y.", <b>N</b>%<b>M</b>=".$n%$m."</p>";
+
+//Doble de cada Variable
 echo"<p><b>El doble de cada Variable:</b></p>";
 echo"<p><b>X</b>=".$x+$x.", <b>Y</b>=".$y+$y.", <b>N</b>=".$n+$n.", <b>M</b>=".$m+$m."</p>";
+
+//Suma de totes les Variables
 echo"<p><b>La suma de totes les Variables:</b></p>";
 echo"<p><b>X</b>+<b>Y</b>+<b>N</b>+<b>M</b>=".$x+$y+$n+$m."</p>";
+
+//Producte de totes les Variables
 echo"<p><b>Producte de totes les Variables:</b></p>";
 echo"<p><b>X</b>*<b>Y</b>*<b>N</b>*<b>M</b>=".$x*$y*$n*$m."</p>";
 ?>
