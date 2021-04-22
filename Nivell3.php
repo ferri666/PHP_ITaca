d7ef9778daa22081abc9d96eb07928194ad6e254
ferri666 <78905276+ferri666@users.noreply.github.com>
2021-04-22 16:25:34 +0200

    Add files via upload

--- Nivell3.php
+++ Nivell3.php
@@ -56,50 +56,50 @@ echo "<p>El total de la seva compra es ".$total_compra."€</p>";
 ///////***Exercici 3***///////////////
 echo"<p><h2>Exercici 3</h2></p>";
 
-//Aquesta funció, donat un número (N), en retorna una array amb tots els prims fins a N
+//Funció que, donat un número (N), ens retorna una array amb tots els prims fins a N
 function Erastotenes($num_max){ 
 
-//Aquesta array la farem servir per descartar els números si son prims o no
-$posibles_prims=array();
+    //Aquesta array la farem servir per descartar els números si son prims o no
+    $posibles_prims=array();
 
-//Aquesta será la array que ens tornará amb tots els números prims que trobem
-$prims_segur=array();
+    //Aquesta será la array que ens tornará amb tots els números prims que trobem
+    $prims_segur=array();
 
-//Primer creem una array amb tots els números del 2 fins a N que hem posat com a índex i un int com a valor
-for($x=2;$x<=$num_max;$x++) {
-  
-    //El valor ens dirá si el número está sense identificar (0), es un número prim (1) o está descartat com a prim (2)
-    //Per tant, tots comencen amb valor 0 (sense identificar)
-    $posibles_prims[$x]=0;
-}
+    //Primer creem una array amb tots els números del 2 fins a N que hem posat com a índex i un int com a valor
+    for($x=2;$x<=$num_max;$x++) {
+    
+        //El valor ens dirá si el número está sense identificar (0), es un número prim (1) o está descartat com a prim (2)
+        //Per tant, tots comencen amb valor 0 (sense identificar)
+        $posibles_prims[$x]=0;
+    }
 
-foreach ($posibles_prims as $key => $value) {
-    global $idx;
+    foreach ($posibles_prims as $key => $value) {
+        global $idx;
 
-    if ($posibles_prims[$key]==0) {
-        //
-        if ($key*$key<$num_max){
+        if ($posibles_prims[$key]==0) {
             //
-            $posibles_prims[$key]=1;
-            array_push($prims_segur, $key);
-            for ($x=$key+1;$x<=$num_max;$x++) {if ($x%$key==0) $posibles_prims[$x]=2;}
-        
-        } else {
-            $idx=$key;
+            if ($key*$key<$num_max){
+                //
+                $posibles_prims[$key]=1;
+                array_push($prims_segur, $key);
+                for ($x=$key+1;$x<=$num_max;$x++) {if ($x%$key==0) $posibles_prims[$x]=2;}
             
-            break;
+            } else {
+                $idx=$key;
+                
+                break;
+            }
+        
+        }
+    }   
+        for ($n=$idx;$n<=$num_max;$n++) if ($posibles_prims[$n]==0) {
+            $posibles_prims[$n]=1;
+            array_push($prims_segur, $n);
         }
     
-    }
-}   
-    for ($n=$idx;$n<=$num_max;$n++) if ($posibles_prims[$n]==0) {
-        $posibles_prims[$n]=1;
-        array_push($prims_segur, $n);
-    }
- 
 
 
-return $prims_segur;
+    return $prims_segur;
 }
 
 
