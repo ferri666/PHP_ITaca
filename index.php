c91e53f9db128cf1d846d71f58eb9a9886201562
ferri666 <78905276+ferri666@users.noreply.github.com>
2021-05-13 18:00:11 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ and open the template in the editor.
             $taula = new Taula;
 
         ?>
-        <h1><i class="fas fa-house-user"></i>  La Compra a Casa  <i class="fas fa-shopping-basket"></i></h1>
+        <h1><i class="fas fa-shopping-basket"></i>  La Compra a Casa  <i class="fas fa-house-user"></i></h1>
         <div class="navbar col-6"><a class="btn btn-primary" href="insereix.php"><i class="fas fa-plus-square"></i> Afegir nou producte</a>
         <button type="button" class="btn btn-success" title="No Hace Nada, es para que la web quede más chula"><i class="fas fa-dollar-sign"></i> Finalitzar compra</button>
         <button type="button" class="btn btn-info" title="No Hace Nada, es para que la web quede más chula"><i class="fas fa-phone-square"></i> Contactar ajuda</button>
